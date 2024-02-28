e5c78460df630ae8e7eab81a17967ccaf9814625
Siva Dinesh K <ksd.ccbp4@gmail.com>
2024-02-28 07:01:30 +0000

    styled

--- src/components/EventItem/index.js
+++ src/components/EventItem/index.js
@@ -1,21 +1,27 @@
 import './index.css'
 
 const EventItem = props => {
-  const {eventDetails, eventClicked} = props
+  const {eventDetails, eventClicked, isClicked} = props
   const {id, imageUrl, name, location} = eventDetails
 
   const onEventClicked = () => {
     eventClicked(id)
   }
 
+  const border = isClicked && 'clicked'
+
   return (
     <li className="event-item">
-      <button type="button" className="event-button" onClick={onEventClicked}>
+      <button
+        type="button"
+        className={`event-button ${border}`}
+        onClick={onEventClicked}
+      >
         <img src={imageUrl} alt="event" className="event-image" />
       </button>
 
       <div className="event-details">
-        <h3 className="event-name">{name}</h3>
+        <p className="event-name">{name}</p>
         <p className="event-location">{location}</p>
       </div>
     </li>
