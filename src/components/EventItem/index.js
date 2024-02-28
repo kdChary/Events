49c07a924f543369aded0c3d47987175c9729bb2
Siva Dinesh K <ksd.ccbp4@gmail.com>
2024-02-28 06:48:44 +0000

    EventItem added

--- src/components/EventItem/index.js
+++ src/components/EventItem/index.js
@@ -1,1 +1,25 @@
-// Write your code here
+import './index.css'
+
+const EventItem = props => {
+  const {eventDetails, eventClicked} = props
+  const {id, imageUrl, name, location} = eventDetails
+
+  const onEventClicked = () => {
+    eventClicked(id)
+  }
+
+  return (
+    <li className="event-item">
+      <button type="button" className="event-button" onClick={onEventClicked}>
+        <img src={imageUrl} alt="event" className="event-image" />
+      </button>
+
+      <div className="event-details">
+        <h3 className="event-name">{name}</h3>
+        <p className="event-location">{location}</p>
+      </div>
+    </li>
+  )
+}
+
+export default EventItem
